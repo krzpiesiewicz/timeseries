e199936dd4b87676dc006002172fcf58a2bc138b
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2022-03-16 00:43:50 +0100

    interval: call added and code reorganization

--- timeseries/interval.py
+++ timeseries/interval.py
@@ -3,22 +3,33 @@ import pandas as pd
 
 
 class Interval:
-    def __init__(self, ts, begin=None, end=None):
+    def __init__(self, ts, begin=None, end=None, as_array=False):
         if type(ts) is np.ndarray:
-            if len(ts.shape) == 1:
-                ts = pd.Series(ts)
-            else:
+            if len(ts.shape) > 1 or as_array:
                 ts = pd.DataFrame(ts)
+            else:
+                ts = pd.Series(ts)
+
+        if type(ts) is pd.Series and as_array:
+            ts = pd.DataFrame(ts)
+
         self.ts = ts
         if begin is not None and end is not None:
             assert begin < end
         self.begin = begin
         self.end = end
+        self.as_array = as_array
 
     def __index__(self, ts=None, begin=None, end=None, prevs=0, nexts=0):
         if ts is None:
             ts = self.ts
         index = ts.index
+        if prevs == "all":
+            begin = None
+            prevs = 0
+        if nexts == "all":
+            end = None
+            nexts = 0
         if begin is not None and begin >= ts.index[0]:
             index = ts.loc[begin:].index
             if prevs > 0:
@@ -36,6 +47,32 @@ class Interval:
             index = index[:nexts]
         return index
 
+    def __call__(self, ts=None, begin=None, end=None, prevs=0, nexts=0,
+                 as_array=None):
+        ts = ts if ts is not None else self.ts
+        begin = begin if begin is not None else self.begin
+        end = end if end is not None else self.end
+        if prevs == "all":
+            begin = None
+            prevs = 0
+        if nexts == "all":
+            end = None
+            nexts = 0
+        as_array = as_array if as_array is not None else self.as_array
+        index = self.__index__(ts, begin, end, prevs, nexts)
+        if begin is not None:
+            begin = index[0]
+        if end is not None:
+            end = index[-1]
+        return Interval(ts, begin, end, as_array)
+
+    def prev(self, ts=None, nexts=0):
+        return self.__call__(ts=ts, end=self.begin, prevs="all",
+                             nexts=nexts + 1)
+
+    def next(self, ts=None, prevs=0):
+        return self.__call__(ts=ts, begin=self.end, prevs=prevs, nexts="all")
+
     def index(self, ts=None, begin=None, end=None, prevs=0, nexts=0):
         if begin is None:
             begin = self.begin
@@ -50,9 +87,15 @@ class Interval:
         ts = ts.loc[index]
         return ts
 
-    def prev_view(self, ts=None, prevs=0):
-        if ts is None:
-            ts = self.ts
-        index = self.__index__(ts, end=self.begin, nexts=prevs)
-        ts = ts.loc[index]
-        return ts
+    def prev_view(self, *args, **kwargs):
+        return self.prev(*args, **kwargs).view()
+
+    def next_view(self, *args, **kwargs):
+        return self.next(*args, **kwargs).view()
+
+    # def prev_view(self, ts=None, prevs=0):
+    #     if ts is None:
+    #         ts = self.ts
+    #     index = self.__index__(ts, end=self.begin, nexts=prevs)
+    #     ts = ts.loc[index]
+    #     return ts
