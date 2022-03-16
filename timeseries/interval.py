4913bcb1d00b99a7a351a6ef90e2220eb6401364
Krzysztof Piesiewicz <krz.piesiewicz@gmail.com>
2022-03-16 09:47:43 +0100

    interval: prev and next bug fixed, and from_intv added to constructor

--- timeseries/interval.py
+++ timeseries/interval.py
@@ -3,7 +3,15 @@ import pandas as pd
 
 
 class Interval:
-    def __init__(self, ts, begin=None, end=None, as_array=False):
+    def __init__(self, ts=None, begin=None, end=None, as_array=False,
+                 from_intv=None):
+        assert ts is not None or from_intv is not None
+        if ts is None:
+            ts = from_intv.ts
+
+        if from_intv is not None:
+            ts = from_intv.view(ts)
+
         if type(ts) is np.ndarray:
             if len(ts.shape) > 1 or as_array:
                 ts = pd.DataFrame(ts)
@@ -13,12 +21,21 @@ class Interval:
         if type(ts) is pd.Series and as_array:
             ts = pd.DataFrame(ts)
 
-        self.ts = ts
         if begin is not None and end is not None:
             assert begin < end
+        # if from_intv is not None:
+        #     whole_ts = from_intv.view(ts)
+        #     intv = from_intv(ts, begin, end, as_array)
+        #     ts = intv.ts
+        #     begin = intv.begin
+        #     end = intv.end
+        self.ts = ts
         self.begin = begin
         self.end = end
         self.as_array = as_array
+
+    def __str__(self):
+        return f"[{self.begin}, {self.end})"
 
     def __index__(self, ts=None, begin=None, end=None, prevs=0, nexts=0):
         if ts is None:
@@ -59,16 +76,17 @@ class Interval:
             end = None
             nexts = 0
         as_array = as_array if as_array is not None else self.as_array
-        index = self.__index__(ts, begin, end, prevs, nexts)
+        index = self.__index__(ts, begin, end, prevs, nexts + 1)
         if begin is not None:
             begin = index[0]
         if end is not None:
             end = index[-1]
+            if end == ts.index[-1]:
+                end = None
         return Interval(ts, begin, end, as_array)
 
     def prev(self, ts=None, nexts=0):
-        return self.__call__(ts=ts, end=self.begin, prevs="all",
-                             nexts=nexts + 1)
+        return self.__call__(ts=ts, end=self.begin, prevs="all")
 
     def next(self, ts=None, prevs=0):
         return self.__call__(ts=ts, begin=self.end, prevs=prevs, nexts="all")
